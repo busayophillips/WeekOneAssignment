8b60537846da23f9db8db7fadaddeec6da522b7c
Busayo Phillips <Busayo@busayo-phillipss-macbook-pro.local>
2018-09-16 13:40:04 -0400

    Initial Commit

---
+++ WeekOneAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeekOneAssignment
+//
+//  Created by Busayo Phillips on 9/16/18.
+//  Copyright © 2018 Busayo Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
