1938d6459b06bb6c7a6a378f27e5da88f5481ccc
Busayo Phillips <Busayo@Busayo-Phillipss-MacBook-Pro.local>
2018-09-20 00:45:58 -0400

    Here is my submission

---
+++ WeekOneAssignment/MovieCell.swift
@@ -0,0 +1,33 @@
+//
+//  MovieCell.swift
+//  WeekOneAssignment
+//
+//  Created by Busayo Phillips on 9/19/18.
+//  Copyright © 2018 Busayo Phillips. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
